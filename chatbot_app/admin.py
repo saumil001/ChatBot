f7a8060b68ffff3eb147348f5f1cec4ba5ac5a66
= <=>
2019-12-20 00:26:12 +0530

    [Modified]: models added

--- chatbot_app/admin.py
+++ chatbot_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ChattingBot
 
 # Register your models here.
+
+admin.site.register(ChattingBot)  # model has been registered to admin
--- chatbot_app/models.py
+++ chatbot_app/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from __future__ import unicode_literals
+from djongo import models
+
 
 # Create your models here.
+
+class ChattingBot(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    name = models.CharField(max_length=20, null=True)
+    age = models.IntegerField(default=10, null=True)
+
+    def __str__(self):
+        return self.name
